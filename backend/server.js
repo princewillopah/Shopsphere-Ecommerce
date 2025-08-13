2d80a702417ac80174f2ba6d1f15b49a4e05c9b7
princewillopah <princewillopah@gmail.com>
2025-08-13 20:19:58 +0100

    new push

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
+// dotenv.config({ path: path.join(__dirname, '../.env.backend') });
 dotenv.config();
 // dotenv.config({ path: path.join(__dirname, '../config/backend.env') });
 
